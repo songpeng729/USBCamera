8f6d5c795149c1d443a88d19ed56434dd829484b
pcjiushizhu <pcjiushizhu@qq.com>
2021-02-19 23:59:11 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,80 @@
+# USBCamera
+
+这是一个USB摄像头APP，基于[UVCCamera](https://github.com/saki4510t/UVCCamera)和[AndroidUSBCamera](https://github.com/jiangdongguo/AndroidUSBCamera)。实现了外接USB摄像头的预览与图片保存功能，同时解决了原项目中一些bug和问题。照片会保存到手机根目录下的USBCamera文件夹下，同时也会保存到相册。
+
+<p align="center">
+<img src="pictures/screenshot.jpg" width="45%" height="45%"/>
+</p>
+
+## 更新历史
+
+### V2.0
+
+此版本弃用了[AndroidUSBCamera](https://github.com/jiangdongguo/AndroidUSBCamera)中的UVCCameraHelper类，只保留了[UVCCamera](https://github.com/saki4510t/UVCCamera)中的核心功能，对无关的代码均做了精简。
+
+其中libusbcamera相机库基于[UVCCamera](https://github.com/saki4510t/UVCCamera)，并添加了USBCameraHelper类简化调用。此外去除了原库中与UI相关的代码，去除了AndoridX依赖。提供了USB相机的连接、预览与图片保存接口。
+
+增加了图像镜像、向左向右旋转，以及预设的四个摄像头角度等功能，适配了外部硬件拍照按钮，同时增加了程序崩溃检测、USB接入设备检测等功能。
+
+
+### V1.0
+
+此版本基于[AndroidUSBCamera](https://github.com/jiangdongguo/AndroidUSBCamera)，对代码中的一些问题进行了改正，开发了一个拥有分辨率选择、对焦、预览与拍照等功能的实例APP。
+
+## 使用方法
+
+### 添加libusbcamera库
+
+将项目源码中的libusbcamera库添加到工程中即可，根据不同的依赖，build.gradle可设置为：
+
+### 使用方法
+
+在Activity中添加如下代码，同时在Layout中设置对应的TextureView。
+
+
+## 兼容性测试
+
+|    机型    |  系统版本  | 安卓版本  | 是否可用 |                           问题描述                           |
+| :--------: | :--------: | :-------: | :------: | :----------------------------------------------------------: |
+| 红米Note4x | Lineage16 | Android9 | 是 | 无 |
+|   小米8    |   MIUI11   | Android10 |    是    |                              无                              |
+|   小米9    |   MIUI11   | Android10 |    是    |                              无                              |
+|  荣耀Note10  |  EMUI9.1   | Android9  |    是    |                              无                              |
+|   荣耀V9   |  EMUI9.1   | Android9  |    是    |                              无                              |
+|  荣耀V10   |  EMUI9.1   | Android9  |    是    |                              无                              |
+|  荣耀V20   | MagicUI2.1 | Android9  |    是    |                              无                              |
+| 荣耀Magic2 | MagicUI2.1 | Android9 | 是 | 无 |
+| 华为MateS  |  EMUI4.0   | Android6  |    否    | 可检测到摄像头插入，但点击连接后无显示。调试后发现调用SO库时报错，暂未定位到问题 |
+| 华为Mate30 |  EMUI10.0  | Android10 |    是    |                              无                              |
+|   华为M3   | EMUI5.0.4  | Android7  |    是    |                              无                              |
+| 一加6T | - | Android9 | 是 | 无 |
+| VIVO X21i | FuntouchOS | Android9 | 否 | 在设置选项中打开OTG后可检测到摄像头，可正确获取分辨率设置，但无画面显示 |
+| VIVO X23 | FuntouchOS | Android9 | 是 | 需要在设置内找到OTG选项并打开 |
+| OPPO K5 | ColorOS6.1 | Android9 | 是 | 需要在设置内找到OTG连接选项并打开 |
+
+## 解决的问题
+
+1. **在部分Android9以上版本无法使用**
+
+此问题是由于Android9中部分设备的UVC摄像头的ID等信息发生了变化，原作者在源码中已修复此问题。
+
+2. **需要手动选择USB设备才可使用**
+
+目前构建的APP，可自动寻找USB摄像头并打开。
+
+3. **Android9中在程序运行时拔出摄像头会导致程序崩溃**
+
+根据Github Pull Request [#454](https://github.com/saki4510t/UVCCamera/pull/454)，重新编译后问题已解决。
+
+4. **JNI库中存在的内存泄露问题**
+
+根据Github Issue [#259](https://github.com/saki4510t/UVCCamera/issues/259)已修改SO库中的空指针错误，重新编译后问题已解决。
+
+5. **解决新版NDK对ABI支持的变动导致JNI编译失败的问题**
+6. **将项目迁移到AndroidX**
+7. **解决Issues [#244](https://github.com/saki4510t/UVCCamera/issues/244)**
+
+## 注意事项
+
+1. 目前编译的目标SDK版本不能超过27，否则会无法正确获取USB摄像头权限，此问题是由于Android9的API特性更改导致的。如果需要支持，可能需要较多修改。
+可参考Pull Request [#480](https://github.com/saki4510t/UVCCamera/pull/480)
---
+++ app/src/main/java/com/shlll/usbcamera/MainActivity.java
@@ -0,0 +1,318 @@
+package com.shlll.usbcamera;
+
+import android.Manifest;
+import android.animation.Animator;
+import android.hardware.usb.UsbDevice;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.serenegiant.widget.UVCCameraTextureView;
+import com.shlll.libusbcamera.USBCameraHelper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.function.Function;
+
+
+public class MainActivity extends AppCompatActivity {
+    private UVCCameraTextureView mUVCCameraView;
+    private USBCameraHelper mUSBCameraHelper;
+    boolean isRightFABOpen = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String[] permission = new String[] {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.VIBRATE
+        };
+        PermissionsUtils.getInstance().chekPermissions(this, permission, permissionsResult);
+
+        mUVCCameraView = (UVCCameraTextureView)findViewById(R.id.camera_view);
+        FrameLayout frame_layout = findViewById(R.id.camera_view_frame);
+        mUSBCameraHelper = new USBCameraHelper(this, mUVCCameraView, frame_layout);
+
+        FloatingActionButton fab_capture = findViewById(R.id.fab_capture),
+                fab_pos = findViewById(R.id.fab_pos),
+                fab_front_up = findViewById(R.id.fab_front_up),
+                fab_front_down = findViewById(R.id.fab_front_down),
+                fab_front_right = findViewById(R.id.fab_front_right),
+                fab_front_left = findViewById(R.id.fab_front_left),
+                fab_back_up = findViewById(R.id.fab_back_up),
+                fab_back_down = findViewById(R.id.fab_back_down),
+                fab_back_right = findViewById(R.id.fab_back_right),
+                fab_back_left = findViewById(R.id.fab_back_left);
+        LinearLayout fab_front_right_layout = findViewById(R.id.fab_front_right_layout),
+                fab_front_left_layout = findViewById(R.id.fab_front_left_layout),
+                fab_front_down_layout = findViewById(R.id.fab_front_down_layout),
+                fab_front_up_layout = findViewById(R.id.fab_front_up_layout),
+                fab_back_right_layout = findViewById(R.id.fab_back_right_layout),
+                fab_back_left_layout = findViewById(R.id.fab_back_left_layout),
+                fab_back_down_layout = findViewById(R.id.fab_back_down_layout),
+                fab_back_up_layout = findViewById(R.id.fab_back_up_layout);
+        TextView  fab_front_right_text = findViewById(R.id.fab_front_right_text),
+                fab_front_left_text = findViewById(R.id.fab_front_left_text),
+                fab_front_down_text = findViewById(R.id.fab_front_down_text),
+                fab_front_up_text = findViewById(R.id.fab_front_up_text),
+                fab_back_right_text = findViewById(R.id.fab_back_right_text),
+                fab_back_left_text = findViewById(R.id.fab_back_left_text),
+                fab_back_down_text = findViewById(R.id.fab_back_down_text),
+                fab_back_up_text = findViewById(R.id.fab_back_up_text);
+
+        fab_front_up_layout.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if (!isRightFABOpen) {
+                    fab_front_right_text.setVisibility(View.GONE);
+                    fab_front_left_text.setVisibility(View.GONE);
+                    fab_front_down_text.setVisibility(View.GONE);
+                    fab_front_up_text.setVisibility(View.GONE);
+                    fab_back_right_text.setVisibility(View.GONE);
+                    fab_back_left_text.setVisibility(View.GONE);
+                    fab_back_down_text.setVisibility(View.GONE);
+                    fab_back_up_text.setVisibility(View.GONE);
+                } else {
+                    fab_front_up.setVisibility(View.VISIBLE);
+                    fab_front_down.setVisibility(View.VISIBLE);
+                    fab_front_right.setVisibility(View.VISIBLE);
+                    fab_front_left.setVisibility(View.VISIBLE);
+                    fab_back_up.setVisibility(View.VISIBLE);
+                    fab_back_down.setVisibility(View.VISIBLE);
+                    fab_back_right.setVisibility(View.VISIBLE);
+                    fab_back_left.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (!isRightFABOpen) {
+                    fab_front_up.setVisibility(View.GONE);
+                    fab_front_down.setVisibility(View.GONE);
+                    fab_front_right.setVisibility(View.GONE);
+                    fab_front_left.setVisibility(View.GONE);
+                    fab_back_up.setVisibility(View.GONE);
+                    fab_back_down.setVisibility(View.GONE);
+                    fab_back_right.setVisibility(View.GONE);
+                    fab_back_left.setVisibility(View.GONE);
+                } else {
+                    fab_front_right_text.setVisibility(View.VISIBLE);
+                    fab_front_left_text.setVisibility(View.VISIBLE);
+                    fab_front_down_text.setVisibility(View.VISIBLE);
+                    fab_front_up_text.setVisibility(View.VISIBLE);
+                    fab_back_right_text.setVisibility(View.VISIBLE);
+                    fab_back_left_text.setVisibility(View.VISIBLE);
+                    fab_back_down_text.setVisibility(View.VISIBLE);
+                    fab_back_up_text.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        Function<Boolean, Boolean> animatePosFabMenu = (open)-> {
+            if(isRightFABOpen == open)
+                return false;
+
+            if(!isRightFABOpen) {
+                isRightFABOpen = true;
+                fab_front_up_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_1));
+                fab_back_up_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_2));
+                fab_front_left_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_3));
+                fab_back_left_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_4));
+                fab_front_right_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_5));
+                fab_back_right_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_6));
+                fab_front_down_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_7));
+                fab_back_down_layout.animate().translationY(-getResources().getDimension(R.dimen.fab_menu_8));
+            } else {
+                isRightFABOpen = false;
+                fab_front_up_layout.animate().translationY(0);
+                fab_back_up_layout.animate().translationY(0);
+                fab_front_left_layout.animate().translationY(0);
+                fab_back_left_layout.animate().translationY(0);
+                fab_front_right_layout.animate().translationY(0);
+                fab_back_right_layout.animate().translationY(0);
+                fab_front_down_layout.animate().translationY(0);
+                fab_back_down_layout.animate().translationY(0);
+            }
+
+            return true;
+        };
+
+        mUSBCameraHelper.setOnCameraButtonListener(new USBCameraHelper.OnCameraButtonListener() {
+            @Override
+            public void onCameraButton() {
+                mUSBCameraHelper.saveCapturePicture();
+            }
+        });
+
+        mUVCCameraView.setOnTransStateChangeListener(new UVCCameraTextureView.OnTransStateChangeListener() {
+            @Override
+            public void onTransStateChange(UVCCameraTextureView.TransState state) {
+                switch (state) {
+                    case CUSTOM:
+                        fab_pos.setImageResource(R.drawable.ic_none);
+                        fab_pos.setRotation(0);
+                        return;
+                    case BACK_UP:
+                        fab_pos.setImageResource(R.drawable.ic_cam_back);
+                        fab_pos.setRotation(0);
+                        break;
+                    case FRONT_UP:
+                        fab_pos.setImageResource(R.drawable.ic_cam_front);
+                        fab_pos.setRotation(0);
+                        break;
+                    case BACK_DOWN:
+                        fab_pos.setImageResource(R.drawable.ic_cam_back);
+                        fab_pos.setRotation(180);
+                        break;
+                    case FRONT_DOWN:
+                        fab_pos.setImageResource(R.drawable.ic_cam_front);
+                        fab_pos.setRotation(180);
+                        break;
+                    case BACK_LEFT:
+                        fab_pos.setImageResource(R.drawable.ic_cam_back);
+                        fab_pos.setRotation(270);
+                        break;
+                    case FRONT_LEFT:
+                        fab_pos.setImageResource(R.drawable.ic_cam_front);
+                        fab_pos.setRotation(270);
+                        break;
+                    case BACK_RIGHT:
+                        fab_pos.setImageResource(R.drawable.ic_cam_back);
+                        fab_pos.setRotation(90);
+                        break;
+                    case FRONT_RIGHT:
+                        fab_pos.setImageResource(R.drawable.ic_cam_front);
+                        fab_pos.setRotation(90);
+                        break;
+                }
+            }
+        });
+
+        frame_layout.setOnClickListener((view)->{
+            animatePosFabMenu.apply(false);
+        });
+
+        fab_capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mUSBCameraHelper.saveCapturePicture();
+            }
+        });
+
+        fab_pos.setOnClickListener((view)->animatePosFabMenu.apply(true ^ isRightFABOpen));
+        fab_front_up.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.FRONT_UP));
+        fab_front_down.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.FRONT_DOWN));
+        fab_front_left.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.FRONT_LEFT));
+        fab_front_right.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.FRONT_RIGHT));
+        fab_back_up.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.BACK_UP));
+        fab_back_down.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.BACK_DOWN));
+        fab_back_left.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.BACK_LEFT));
+        fab_back_right.setOnClickListener((view)->mUSBCameraHelper.setPosition(UVCCameraTextureView.TransState.BACK_RIGHT));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionsUtils.getInstance().onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(getResources().getString(R.string.diag_about_title))
+                    .setMessage(getResources().getString(R.string.diag_about_message));
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else if (id == R.id.action_usbinfo) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(getResources().getString(R.string.diag_usbinfo_title));
+
+            String diag_str = "";
+            final List<UsbDevice> device_list =  mUSBCameraHelper.getDeviceList();
+
+            for (UsbDevice device: device_list) {
+                diag_str += "Class:" + device.getDeviceClass()
+                        + ",subClass:" + device.getDeviceSubclass()
+                        + ",VID:" + device.getVendorId()
+                        + ",PID:" + device.getProductId() + "\n";
+            }
+
+            builder.setMessage(diag_str);
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        mUSBCameraHelper.destory();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mUSBCameraHelper.start();
+    }
+
+    @Override
+    protected void onStop() {
+        mUSBCameraHelper.stop();
+        super.onStop();
+    }
+
+    private PermissionsUtils.IPermissionsResult permissionsResult = new PermissionsUtils.IPermissionsResult() {
+        @Override
+        public void passPermissons() {
+        }
+
+        @Override
+        public void forbitPermissons() {
+        }
+    };
+}
